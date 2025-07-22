4f908f7197933043fdc0dc863e088c747b725cc3
bahareh <baharehviani@gmail.com>
2025-07-22 23:12:40 +0330

    cors detailes removed

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const app = express();
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
-app.use(cors({
-  origin: 'https://collegium.kojiberi.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 
